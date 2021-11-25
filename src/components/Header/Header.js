dbf6f6afe24ab5d746c1fb001fe4ed2d42779ed5
Edwin Orellana <educsis@gmail.com>
2021-11-25 09:55:40 -0800

    Updating site

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import { FaShoppingCart } from 'react-icons/fa';
+import Container from '@components/Container';
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return(
+        <header className={styles.header}>
+            <Container className={styles.headerContainer}>
+                <p className={styles.headerTitle}>
+                    Edwinsito Store
+                </p>
+                <p className={styles.headerCart}>
+                    <FaShoppingCart />
+                    $0.00
+                </p>
+            </Container>
+        </header>
+    )
+}
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '@components/Header'
 import styles from '@styles/Home.module.scss'
 
 export default function Home() {
@@ -11,11 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Edwinsito Store
-        </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
@@ -53,16 +51,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        &copy; My Store, {new Date().getFullYear()}
       </footer>
     </div>
   )
